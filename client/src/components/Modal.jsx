5b8040309e21ec95d09b8282620fd124812566e3
Harry Cooper <harry@bigbite.net>
2025-05-17 02:29:07 +0100

    feat: enhance Modal component to accept custom class names and improve styles

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,14 +1,15 @@
 /**
  * Modal component
  *
- * @param {object}    props          - Props for the component
- * @param {string}    props.title    - Title of the modal
- * @param {Function}  props.onClose  - Function to close the modal
- * @param {ReactNode} props.children - Content to be displayed inside the modal
+ * @param {object}    props           - Props for the component
+ * @param {string}    props.title     - Title of the modal
+ * @param {Function}  props.onClose   - Function to close the modal
+ * @param {ReactNode} props.children  - Content to be displayed inside the modal
+ * @param {string=}   props.className - Additional class names for styling
  *
  * @returns {JSX.Element} - Rendered component
  */
-const Modal = ({ title, onClose, children }) => {
+const Modal = ({ title, onClose, children, className = '' }) => {
 	return (
 		<div
 			className="modal__background"
@@ -16,7 +17,7 @@ const Modal = ({ title, onClose, children }) => {
 				e.target.classList.contains('modal__background') && onClose()
 			}
 		>
-			<div className="modal">
+			<div className={`modal ${className}`}>
 				<div className="modal__header">
 					<h2 className="modal__title">{title}</h2>
 					<button className="modal__close" onClick={onClose}>
