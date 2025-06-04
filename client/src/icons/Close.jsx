55165c5521ee8bfea5c72b97357c309570b3f24d
Harry Cooper <harry@bigbite.net>
2025-06-05 00:48:55 +0100

    feat: add notice icon

---
+++ client/src/icons/Close.jsx
@@ -0,0 +1,23 @@
+/**
+ * Close Component
+ *
+ * @param {object} props           - Component properties
+ * @param {string} props.className - Additional class names
+ *
+ * @returns {JSX.Element} - Rendered Edit icon
+ */
+const Close = ({ className }) => {
+	return (
+		<svg
+			width="800"
+			height="800"
+			viewBox="0 0 32 32"
+			xmlns="http://www.w3.org/2000/svg"
+			className={className}
+		>
+			<path d="M18.8 16l5.5-5.5c.8-.8.8-2 0-2.8-.3-.4-.8-.7-1.3-.7s-1 .2-1.4.6L16 13.2l-5.5-5.5c-.8-.8-2.1-.8-2.8 0-.4.3-.7.8-.7 1.4s.2 1 .6 1.4l5.5 5.5-5.5 5.5c-.3.4-.6.9-.6 1.5 0 .5.2 1 .6 1.4.4.4.9.6 1.4.6.5 0 1-.2 1.4-.6l5.5-5.5 5.5 5.5c.8.8 2.1.8 2.8 0 .8-.8.8-2.1 0-2.8L18.8 16z" />
+		</svg>
+	);
+};
+
+export default Close;
